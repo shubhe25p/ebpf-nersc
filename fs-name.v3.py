0abfc8b54e729c7c537f982198800db053a697c4
Shubh Pachchigar <33875085+shubhe25p@users.noreply.github.com>
2025-03-19 02:11:29 +0000

    more complicateed way

--- fs-name.v3.py
+++ fs-name.v3.py
@@ -1,31 +1,37 @@
 from bcc import BPF
 import time
 
-
-
 bpf_text="""
 #include <linux/sched.h>
+#include <linux/mount.h>
+#include <linux/path.h>
 #include <linux/fdtable.h>
 #include <linux/fs.h>
+#include <linux/dcache.h>
 
-void trace_sys_enter(void* ctx)
+TRACEPOINT_PROBE(syscalls, sys_enter_read)
 {
+    char fsname[32];
+    struct fs_key key = {};
+    struct fd_info info = {};
+    // think of storing fd somewhere
     struct task_struct *task = (struct task_struct *)bpf_get_current_task();
-    struct files_struct *fs = (struct files_struct* )task->files;
-    struct fdtable *fdt = (struct fdtable *)fs->fdt;
-    struct file **fd = (struct file **)fdt->fd;
-    bpf_trace_printk("onCPU %d with flags: %u\\n", task->on_cpu, fd[0]->f_flags);
+    const unsigned char *fs_name = task->files->fdt->fd[args->fd].f_path->mnt->mnt_root->d_name.name;
+    bpf_probe_read_kernel_str(&key.fsname, sizeof(key.fsname), fs_name);
+    u64 ts = bpf_ktime_get_ns();
+    u64 ts = bpf_ktime_get_ns();
+    
+    bpf_trace_printk("Process %d is using file system: %s\\n", task->pid, fsname);
+    return 0;
 }
 """
 
 b = BPF(text=bpf_text)
 
-b.attach_tracepoint(tp="syscalls:sys_enter_openat", fn_name="trace_sys_enter")
-
 print("Tracing syscalls .. Press Ctrl-C to end.")
 
 # Format output
-print("%-18s %-6s %s" % ("TIME(s)", "ONCPU", "USERS"))
+print("%-18s %-6s %s" % ("TIME(s)", "PID", "FSNAME"))
 
 
 # Trace output
@@ -35,12 +41,12 @@ try:
         (task, pid, cpu, flags, ts, msg) = b.trace_fields()
         elapsed = time.time() - start_time
         
-        msg_str = msg.decode('utf-8', 'replace')
-        if "onCPU" in msg_str:
-            parts = msg_str.split("with flags: ")
-            if len(parts) == 2:
-                oncpu_part = parts[0].split("onCPU ")[1].strip()
-                users = parts[1].strip()
-                print("%-18.9f %-6s %s" % (elapsed, oncpu_part, users))
+        fields = msg.decode('utf-8', 'replace').split("is using file system: ")
+        if len(fields) == 2:
+            pid_info = fields[0].strip().split()
+            if len(pid_info) >= 2:
+                pid = pid_info[1]
+                fs_name = fields[1]
+                print("%-18.9f %-6s %s" % (elapsed, pid, fs_name))
 except KeyboardInterrupt:
     print("\nTracing stopped.")
