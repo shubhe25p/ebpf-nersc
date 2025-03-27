fb856cb5ce1fc17fcfb63be85ac01e47afda7551
Shubh Pachchigar <33875085+shubhe25p@users.noreply.github.com>
2025-03-27 00:00:22 +0000

    bug:

--- fs-name.v3.py
+++ fs-name.v3.py
@@ -2,21 +2,58 @@ from bcc import BPF
 import time
 
 bpf_text="""
+#include <uapi/linux/ptrace.h>
+#include <linux/blk-mq.h>
+#include <uapi/linux/ptrace.h>
+#include <linux/fs.h>
+#include <linux/dcache.h>
 #include <linux/sched.h>
 #include <linux/mount.h>
 #include <linux/path.h>
-#include <linux/fdtable.h>
-#include <linux/fs.h>
-#include <linux/dcache.h>
+#include <linux/fs_struct.h>
 
 TRACEPOINT_PROBE(syscalls, sys_enter_read)
 {
+    struct task_struct *task;
+    struct fs_struct *fs;
+    struct path pwd_path;
+    struct vfsmount *mnt;
+    struct super_block *superblock;
+    struct file_system_type *fstype;
+    struct dentry *mnt_point;
+    const char *fsname_ptr;
     char fsname[32];
-    // think of storing fd somewhere
-    struct task_struct *task = (struct task_struct *)bpf_get_current_task();
-    const unsigned char *fs_name = task->files->fdt->fd[args->fd]->f_path.mnt->mnt_root->d_name.name;
-    bpf_probe_read_kernel_str(&fsname, sizeof(fsname), fs_name);
-    u64 ts = bpf_ktime_get_ns();
+    struct qstr dname;
+    struct key_t key = {};
+
+    
+
+    // Get current task_struct
+    task = (struct task_struct *)bpf_get_current_task();
+    // Read task->fs
+    bpf_probe_read_kernel(&fs, sizeof(fs), &task->fs);
+    if (!fs)
+        return 0;
+
+    // Read fs->pwd (current working directory)
+    bpf_probe_read_kernel(&pwd_path, sizeof(pwd_path), &fs->pwd);
+
+    // Read pwd_path.mnt (vfsmount)
+    bpf_probe_read_kernel(&mnt, sizeof(mnt), &pwd_path.mnt);
+    if (!mnt)
+        return 0;
+    
+
+    bpf_probe_read_kernel(&mnt_point, sizeof(mnt_point), &mnt->mnt_root);
+    if (!mnt_point)
+        return 0;
+    
+    // read vfs_mount dentry
+    bpf_probe_read_kernel(&dname, sizeof(dname), &mnt_point->d_name);
+    
+    // read dentry qstr
+    bpf_probe_read_kernel(&fsname_ptr, sizeof(fsname_ptr), &dname.name);
+    bpf_probe_read_kernel_str(&key.fsname, sizeof(key.fsname), fsname_ptr);
     
     bpf_trace_printk("Process %d is using file system: %s\\n", task->pid, fsname);
     return 0;
