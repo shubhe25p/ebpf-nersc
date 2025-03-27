5e112214d771ee77692bd68b14b463a5cc29a266
Shubh Pachchigar <33875085+shubhe25p@users.noreply.github.com>
2025-03-27 00:12:50 +0000

    bug

--- fs-name.v3.py
+++ fs-name.v3.py
@@ -6,6 +6,7 @@ bpf_text="""
 #include <linux/blk-mq.h>
 #include <uapi/linux/ptrace.h>
 #include <linux/fs.h>
+#include <linux/fdtable.h>
 #include <linux/dcache.h>
 #include <linux/sched.h>
 #include <linux/mount.h>
@@ -15,7 +16,9 @@ bpf_text="""
 TRACEPOINT_PROBE(syscalls, sys_enter_read)
 {
     struct task_struct *task;
-    struct fs_struct *fs;
+    struct files_struct *fs;
+    struct fdtable *fdt;
+    struct file **fd;
     struct path pwd_path;
     struct vfsmount *mnt;
     struct super_block *superblock;
@@ -29,31 +32,20 @@ TRACEPOINT_PROBE(syscalls, sys_enter_read)
     // Get current task_struct
     task = (struct task_struct *)bpf_get_current_task();
     // Read task->fs
-    bpf_probe_read_kernel(&fs, sizeof(fs), &task->fs);
+    bpf_probe_read_kernel(&fs, sizeof(fs), &task->files);
     if (!fs)
         return 0;
+    
+    bpf_probe_read_kernel(&fdt, sizeof(fdt), &fs->fdt);
+    if (!fdt)
+        return 0;
 
-    // Read fs->pwd (current working directory)
-    bpf_probe_read_kernel(&pwd_path, sizeof(pwd_path), &fs->pwd);
-
-    // Read pwd_path.mnt (vfsmount)
-    bpf_probe_read_kernel(&mnt, sizeof(mnt), &pwd_path.mnt);
-    if (!mnt)
+    bpf_probe_read_kernel(&fdt, sizeof(fdt), &fdt->fd[args->fd]);
+    if (!fdt)
         return 0;
     
-
-    bpf_probe_read_kernel(&mnt_point, sizeof(mnt_point), &mnt->mnt_root);
-    if (!mnt_point)
-        return 0;
     
-    // read vfs_mount dentry
-    bpf_probe_read_kernel(&dname, sizeof(dname), &mnt_point->d_name);
-    
-    // read dentry qstr
-    bpf_probe_read_kernel(&fsname_ptr, sizeof(fsname_ptr), &dname.name);
-    bpf_probe_read_kernel_str(&fsname, sizeof(fsname), fsname_ptr);
-    
-    bpf_trace_printk("Process %d is using file system: %s\\n", task->pid, fsname);
+    bpf_trace_printk("Process %d is using file system: %s\\n", task->pid, args->fd);
     return 0;
 }
 """
@@ -61,24 +53,3 @@ TRACEPOINT_PROBE(syscalls, sys_enter_read)
 b = BPF(text=bpf_text)
 
 print("Tracing syscalls .. Press Ctrl-C to end.")
-
-# Format output
-print("%-18s %-6s %s" % ("TIME(s)", "PID", "FSNAME"))
-
-
-# Trace output
-start_time = time.time()
-try:
-    while True:
-        (task, pid, cpu, flags, ts, msg) = b.trace_fields()
-        elapsed = time.time() - start_time
-        
-        fields = msg.decode('utf-8', 'replace').split("is using file system: ")
-        if len(fields) == 2:
-            pid_info = fields[0].strip().split()
-            if len(pid_info) >= 2:
-                pid = pid_info[1]
-                fs_name = fields[1]
-                print("%-18.9f %-6s %s" % (elapsed, pid, fs_name))
-except KeyboardInterrupt:
-    print("\nTracing stopped.")
