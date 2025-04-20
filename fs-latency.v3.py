fad3fe378824309d0f80d37e5a7a10b800cc4a5d
Shubh Pachchigar <33875085+shubhe25p@users.noreply.github.com>
2025-04-20 01:23:34 +0000

    bug fix test

--- fs-latency.v3.py
+++ fs-latency.v3.py
@@ -48,7 +48,10 @@ struct mount{
     struct vfsmount mnt;
     const char *mnt_devname;
 };
-
+#define offsetof(TYPE, MEMBER) ((size_t)&((TYPE *)0)->MEMBER)
+#define container_of(ptr, type, member) \
+    ((type *)((char *)(ptr) - offsetof(type, member)))
+    
 struct fs_stat_t {
     u64 bucket;
     u64 ts;
