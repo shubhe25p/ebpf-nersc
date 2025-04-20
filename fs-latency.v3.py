cd2038a1d3c93b872f433bcce49360515b3450ba
Shubh Pachchigar <33875085+shubhe25p@users.noreply.github.com>
2025-04-20 00:44:20 +0000

    bug fix

--- fs-latency.v3.py
+++ fs-latency.v3.py
@@ -60,9 +60,10 @@ struct fs_stat_t {
     char comm[TASK_COMM_LEN];
 };
 
-#define containerof(ptr, type, member) ({                   
-    const typeof( ((type *)0)->member ) *__mptr = (ptr);   
-    (type *)( (char *)__mptr - offsetof(type,member) );})
+#define container_of(ptr, type, member) ({                \
+    const typeof(((type *)0)->member) *__mptr = (ptr);    \
+    (type *)((char *)__mptr - offsetof(type, member)); })
+
 
 
 BPF_HASH(read_start, pid_t, struct fs_stat_t);
