d7ba15a0bb85a86de980b967b8f14ee84d078728
Shubh Pachchigar <33875085+shubhe25p@users.noreply.github.com>
2025-04-22 15:20:23 +0000

    iops tp

---
+++ proc-io-wait.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+import sys
+import time
+
+from bcc import BPF
+
+src = r"""
+BPF_RINGBUF_OUTPUT(buffer, 1 << 4);
+
+struct event {
+    u32 pid;
+    u32 tgid;
+    int dfd;
+    int flags;
+    int mode;
+};
+
+TRACEPOINT_PROBE(syscalls, sys_enter_read) {
+    int zero = 0;
+
+    struct event event = {};
+    event.dfd = args->dfd;
+    event.flags = args->flags;
+    event.mode = args->mode;
+    event.pid = bpf_get_current_pid_tgid();
+    event.tgid = bpf_get_current_pid_tgid() >> 32;
+
+    buffer.ringbuf_output(&event, sizeof(event), 0);
+
+    return 0;
+}
+"""
+
+b = BPF(text=src)
+
+def callback(ctx, data, size):
+    event = b['buffer'].event(data)
+    print("%10d %10d %10d" % event.dfd, event.flags, event.tgid))
+
+b['buffer'].open_ring_buffer(callback)
+
+print("Printing read() calls, ctrl-c to exit.")
+
+print("%10s %10s %10s" % ("DIR_FD", "FLAGS", "PID"))
+
+try:
+    while 1:
+        b.ring_buffer_poll()
+        # or b.ring_buffer_consume()
+        time.sleep(0.5)
+except KeyboardInterrupt:
+    sys.exit()
