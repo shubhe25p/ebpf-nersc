b27fbb3477daf8a11ef65683c19d5aae04936cb8
Shubh Pachchigar <33875085+shubhe25p@users.noreply.github.com>
2025-05-02 06:36:02 +0000

    try blocking mkdir

--- try_block_syscall.py
+++ try_block_syscall.py
@@ -1,30 +1,16 @@
-#!/usr/bin/python
-#
-# This is a Hello World example that formats output as fields.
-
+#!/usr/bin/env python
 from bcc import BPF
-from bcc.utils import printb
 
-# define BPF program
-prog = """
-int kprobe__x64_sys_call(struct pt_regs *ctx) {
-    bpf_trace_printk("Hello, World! %d \\n", PT_REGS_PARM2(ctx));
+# bpf program: intercept mkdirat and force it to return -EPERM
+bpf_text = """
+#include <uapi/linux/ptrace.h>
+int kprobe__sys_mkdirat(struct pt_regs *ctx, int dfd, const char __user *pathname, umode_t mode) {
+    bpf_trace_printk("mkdir blocked on this machine: %s\\n", pathname);
+    bpf_override_return(ctx, -EPERM);
     return 0;
 }
 """
 
-# load BPF program
-b = BPF(text=prog)
-
-# header
-print("%-18s %-16s %-6s %s" % ("TIME(s)", "COMM", "PID", "MESSAGE"))
-
-# format output
-while 1:
-    try:
-        (task, pid, cpu, flags, ts, msg) = b.trace_fields()
-    except ValueError:
-        continue
-    except KeyboardInterrupt:
-        exit()
-    printb(b"%-18.9f %-16s %-6d %s" % (ts, task, pid, msg))
+b = BPF(text=bpf_text)
+print("Blocking mkdir… Press Ctrl+C to exit.")
+b.trace_print()
