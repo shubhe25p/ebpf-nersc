55fd245d7f28510e79ac22ce9294a65bac436532
shubh@DOE <shubhp@mbm3a24.local>
2025-06-08 17:47:20 -0700

    update types and helpers

--- bpf_helpers/trace1.user.c
+++ bpf_helpers/trace1.user.c
@@ -1,51 +1,27 @@
-// trace1.c make sure to gen skel file before running this
 #include <stdio.h>
-#include <stdlib.h>
-#include <signal.h>
 #include <unistd.h>
-#include <bpf/libbpf.h>
-#include "trace1.skel.h"
+#include <stdint.h>
+#include "trace1_openat.skel.h"   // your bpftool-gen’d header
 
-static volatile sig_atomic_t exiting = 0;
-
-void handle_signal(int sig) {
-    exiting = 1;
-}
-
-int main(int argc, char **argv) {
-    struct trace1_bpf *skel;
-    int map_fd;
+int main(void) {
+    struct trace1_openat_bpf *skel;
+    uint32_t key = 0, next_key;
+    uint64_t value;
     int err;
 
-    signal(SIGINT, handle_signal);
-    signal(SIGTERM, handle_signal);
+    skel = trace1_openat_bpf__open_and_load();
+    if (!skel) return 1;
+    err = trace1_openat_bpf__attach(skel);
+    if (err) goto cleanup;
 
-    skel = trace1_bpf__open_and_load();
-    if (!skel) {
-        fprintf(stderr, "Failed to open and load BPF skeleton\n");
-        return 1;
-    }
-    err = trace1_bpf__attach(skel);
-    if (err) {
-        fprintf(stderr, "Failed to attach BPF skeleton\n");
-        goto cleanup;
-    }
-
-    map_fd = bpf_map__fd(skel->maps.cnt);
-
-    printf("Polling cnt map every 5 seconds. Ctrl+C to exit.\n");
-    while (!exiting) {
-        sleep(5);
-        __u32 key = 0, next_key;
-        __u64 value;
-        while (bpf_map__get_next_key(map_fd, &key, &next_key) == 0) {
-            if (bpf_map__lookup_elem(map_fd, &next_key, &value) == 0)
-                printf("key=%u val=%llu\n", next_key, value);
-            key = next_key;
-        }
+    printf("Polling… CTRL-C to exit\n");
+    while (bpf_map__get_next_key(skel->maps.cnt, &key, &next_key) == 0) {
+        if (bpf_map__lookup_elem(skel->maps.cnt, &next_key, &value) == 0)
+            printf("openat calls: %llu\n", value);
+        key = next_key;
     }
 
 cleanup:
-    trace1_bpf__destroy(skel);
-    return 0;
+    trace1_openat_bpf__destroy(skel);
+    return err;
 }
