7c07feb228e748a04786e2f69531e60800b60e09
shubh@DOE <shubhp@mbm3a24.local>
2025-06-11 23:32:06 -0700

    fix issues

--- bpf_helpers/trace2.bpf.c
+++ bpf_helpers/trace2.bpf.c
@@ -3,7 +3,7 @@
 #include <bpf/bpf_core_read.h>
 
 SEC("tracepoint/syscalls/sys_enter_execve")
-int trace_execve(struct pt_regs *ctx, const char *filename)
+int bpf_prog1(struct pt_regs *ctx, const char *filename)
 {
     char fname[256];
 
