c345bde5a6ddaccbce281ad7e6ee9a246e5a8c21
shubh@DOE <shubhp@mbm3a24.local>
2025-06-11 23:31:09 -0700

    fix issues

--- bpf_helpers/trace2.bpf.c
+++ bpf_helpers/trace2.bpf.c
@@ -5,11 +5,10 @@
 SEC("tracepoint/syscalls/sys_enter_execve")
 int trace_execve(struct pt_regs *ctx, const char *filename)
 {
-    char filename[256];
-    const char *fname = (const char *)(filename);
+    char fname[256];
 
     /* safely read user-space filename */
-    if (bpf_core_read_user_str(filename, sizeof(filename), fname) > 0)
+    if (bpf_core_read_user_str(fname, sizeof(fname), filename) > 0)
         bpf_printk("execve: %s\n", filename);
 
     return 0;
