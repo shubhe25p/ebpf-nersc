3c205cc59a7df5c782f98219475ce357e66474dd
shubh@DOE <shubhp@mbm3a24.local>
2025-06-12 00:19:55 -0700

    fix issues

--- bpf_helpers/trace2.bpf.c
+++ bpf_helpers/trace2.bpf.c
@@ -14,7 +14,7 @@ int bpf_prog1(struct syscall_trace_enter* ctx)
     int len=0;
     for(int i=0;i<256;i++)
     {
-        if(fname[i]=='0')
+        if(fname[i]!='0')
             len++;
     }
     bpf_printk("filename %s with len %d \n", fname, len);
