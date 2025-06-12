6154cab64cbab4447cbe692cf10952ccd2fc859b
shubh@DOE <shubhp@mbm3a24.local>
2025-06-11 23:26:39 -0700

    init

--- bpf_helpers/trace2.bpf.c
+++ bpf_helpers/trace2.bpf.c
@@ -1,48 +1,18 @@
 #include "vmlinux.h"
-#include <linux/version.h>
 #include <bpf/bpf_helpers.h>
-#include <bpf/bpf_tracing.h>
+#include <bpf/bpf_core_read.h>
 
-struct pair {
-	u64 val;
-	u64 ip;
-};
-
-struct {
-	__uint(type, BPF_MAP_TYPE_HASH);
-	__type(key, long);
-	__type(value, struct pair);
-	__uint(max_entries, 1000000);
-} my_map SEC(".maps");
-
-/* kprobe is NOT a stable ABI. If kernel internals change this bpf+kprobe
- * example will no longer be meaningful
- */
-SEC("kprobe/kmem_cache_free")
-int bpf_prog1(struct pt_regs *ctx)
+SEC("tracepoint/syscalls/sys_enter_execve")
+int trace_execve(struct pt_regs *ctx, const char __user *filename)
 {
-	long ptr = PT_REGS_PARM2(ctx);
+    char filename[256];
+    const char *fname = (const char *)(filename);
 
-	bpf_map_delete_elem(&my_map, &ptr);
-	return 0;
+    /* safely read user-space filename */
+    if (bpf_core_read_user_str(filename, sizeof(filename), fname) > 0)
+        bpf_printk("execve: %s\n", filename);
+
+    return 0;
 }
 
-SEC("kretprobe/kmem_cache_alloc_node_noprof")
-int bpf_prog2(struct pt_regs *ctx)
-{
-	long ptr = PT_REGS_RC(ctx);
-	long ip = 0;
-
-	/* get ip address of kmem_cache_alloc_node_noprof() caller */
-	BPF_KRETPROBE_READ_RET_IP(ip, ctx);
-
-	struct pair v = {
-		.val = bpf_ktime_get_ns(),
-		.ip = ip,
-	};
-
-	bpf_map_update_elem(&my_map, &ptr, &v, BPF_ANY);
-	return 0;
-}
-char _license[] SEC("license") = "GPL";
-u32 _version SEC("version") = LINUX_VERSION_CODE;
+char LICENSE[] SEC("license") = "GPL";
--- bpf_helpers/trace2.user.c
+++ bpf_helpers/trace2.user.c
@@ -1,54 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <signal.h>
 #include <unistd.h>
-#include <stdbool.h>
-#include <string.h>
-#include <time.h>
-
-#include <bpf/bpf.h>
 #include <bpf/libbpf.h>
-
-struct pair {
-	long long val;
-	__u64 ip;
-};
-
-static __u64 time_get_ns(void)
-{
-	struct timespec ts;
-
-	clock_gettime(CLOCK_MONOTONIC, &ts);
-	return ts.tv_sec * 1000000000ull + ts.tv_nsec;
-}
-
-static void print_old_objects(int fd)
-{
-	long long val = time_get_ns();
-	__u64 key, next_key;
-	struct pair v;
-
-	key = write(1, "\e[1;1H\e[2J", 11); /* clear screen */
-
-	key = -1;
-	while (bpf_map_get_next_key(fd, &key, &next_key) == 0) {
-		bpf_map_lookup_elem(fd, &next_key, &v);
-		key = next_key;
-		if (val - v.val < 1000000000ll)
-			/* object was allocated more then 1 sec ago */
-			continue;
-		printf("obj 0x%llx is %2lldsec old was allocated at ip %llx\n",
-		       next_key, (val - v.val) / 1000000000ll, v.ip);
-	}
-}
+#include "trace_helpers.h"
 
 int main(int ac, char **argv)
 {
-	struct bpf_link *links[2];
+	struct bpf_link *link = NULL;
 	struct bpf_program *prog;
 	struct bpf_object *obj;
 	char filename[256];
-	int map_fd, j = 0;
 
 	snprintf(filename, sizeof(filename), "%s.bpf.o", argv[0]);
 	obj = bpf_object__open_file(filename, NULL);
@@ -57,37 +17,29 @@ int main(int ac, char **argv)
 		return 0;
 	}
 
+	prog = bpf_object__find_program_by_name(obj, "bpf_prog1");
+	if (!prog) {
+		fprintf(stderr, "ERROR: finding a prog in obj file failed\n");
+		goto cleanup;
+	}
+
 	/* load BPF program */
 	if (bpf_object__load(obj)) {
 		fprintf(stderr, "ERROR: loading BPF object file failed\n");
 		goto cleanup;
 	}
 
-	map_fd = bpf_object__find_map_fd_by_name(obj, "my_map");
-	if (map_fd < 0) {
-		fprintf(stderr, "ERROR: finding a map in obj file failed\n");
+	link = bpf_program__attach(prog);
+	if (libbpf_get_error(link)) {
+		fprintf(stderr, "ERROR: bpf_program__attach failed\n");
+		link = NULL;
 		goto cleanup;
 	}
 
-	bpf_object__for_each_program(prog, obj) {
-		links[j] = bpf_program__attach(prog);
-		if (libbpf_get_error(links[j])) {
-			fprintf(stderr, "ERROR: bpf_program__attach failed\n");
-			links[j] = NULL;
-			goto cleanup;
-		}
-		j++;
-	}
-
-	while (1) {
-		print_old_objects(map_fd);
-		sleep(1);
-	}
+	read_trace_pipe();
 
 cleanup:
-	for (j--; j >= 0; j--)
-		bpf_link__destroy(links[j]);
-
+	bpf_link__destroy(link);
 	bpf_object__close(obj);
 	return 0;
 }
