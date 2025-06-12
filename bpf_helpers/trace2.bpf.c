7683c8565ff3b0893c84fda411f2a2f548327b44
shubh@DOE <shubhp@mbm3a24.local>
2025-06-12 00:03:42 -0700

    fix issues

--- bpf_helpers/trace2.bpf.c
+++ bpf_helpers/trace2.bpf.c
@@ -3,12 +3,12 @@
 #include <bpf/bpf_core_read.h>
 
 SEC("tracepoint/syscalls/sys_enter_execve")
-int bpf_prog1(struct pt_regs *ctx)
+int bpf_prog1(struct syscall_trace_enter* ctx)
 {
     char fname[256];
 
     if (BPF_CORE_READ_STR_INTO(fname, sizeof(fname), (const char*)ctx->args[0]) > 0)
-        bpf_printk("execve: %s\n", fp);
+        bpf_printk("execve: %s\n", fnmaw);
 
     return 0;
 }
